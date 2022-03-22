b5d58f625a8216a0ab332af4ae7a990918b6a9f2
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-03-22 19:28:06 +0800

    implement business logic for ImageTypeManagementOperator

--- src/module/image_types/image_type_management_operator.ts
+++ src/module/image_types/image_type_management_operator.ts
@@ -2,7 +2,12 @@ import { injected, token } from "brandi";
 import { Logger } from "winston";
 import { IMAGE_SERVICE_DM_TOKEN } from "../../dataaccess/grpc";
 import { ImageServiceClient } from "../../proto/gen/ImageService";
-import { LOGGER_TOKEN } from "../../utils";
+import {
+    ErrorWithHTTPCode,
+    getHttpCodeFromGRPCStatus,
+    LOGGER_TOKEN,
+    promisifyGRPCCall,
+} from "../../utils";
 import { ImageType, RegionLabel } from "../schemas";
 
 export interface ImageTypeManagementOperator {
@@ -34,7 +39,7 @@ export interface ImageTypeManagementOperator {
     removeRegionLabelFromImageType(
         imageTypeID: number,
         regionLabelID: number
-    ): Promise<RegionLabel>;
+    ): Promise<void>;
 }
 
 export class ImageTypeManagementOperatorImpl
@@ -49,14 +54,62 @@ export class ImageTypeManagementOperatorImpl
         displayName: string,
         hasPredictiveModel: boolean
     ): Promise<ImageType> {
-        throw new Error("Method not implemented.");
+        const {
+            error: createImageTypeError,
+            response: createImageTypeResponse,
+        } = await promisifyGRPCCall(
+            this.imageServiceDM.createImageType.bind(this.imageServiceDM),
+            { displayName, hasPredictiveModel }
+        );
+        if (createImageTypeError !== null) {
+            this.logger.error(
+                "failed to call image_service.createImageType()",
+                { error: createImageTypeError }
+            );
+            throw new ErrorWithHTTPCode(
+                "failed to create new image type",
+                getHttpCodeFromGRPCStatus(createImageTypeError.code)
+            );
+        }
+
+        return ImageType.fromProto(createImageTypeResponse?.imageType);
     }
 
     public async getImageTypeList(withRegionLabel: boolean): Promise<{
         imageTypeList: ImageType[];
         regionLabelList: RegionLabel[][] | undefined;
     }> {
-        throw new Error("Method not implemented.");
+        const {
+            error: getImageTypeListError,
+            response: getImageTypeListResponse,
+        } = await promisifyGRPCCall(
+            this.imageServiceDM.getImageTypeList.bind(this.imageServiceDM),
+            { withRegionLabel }
+        );
+        if (getImageTypeListError !== null) {
+            this.logger.error(
+                "failed to call image_service.getImageTypeList()",
+                { error: getImageTypeListError }
+            );
+            throw new ErrorWithHTTPCode(
+                "failed to get image type list",
+                getHttpCodeFromGRPCStatus(getImageTypeListError.code)
+            );
+        }
+
+        return {
+            imageTypeList:
+                getImageTypeListResponse?.imageTypeList?.map(
+                    ImageType.fromProto
+                ) || [],
+            regionLabelList:
+                getImageTypeListResponse?.regionLabelListOfImageTypeList?.map(
+                    (regionLabelList) =>
+                        regionLabelList.regionLabelList?.map(
+                            RegionLabel.fromProto
+                        ) || []
+                ),
+        };
     }
 
     public async updateImageType(
@@ -64,11 +117,42 @@ export class ImageTypeManagementOperatorImpl
         displayName: string | undefined,
         hasPredictiveModel: boolean | undefined
     ): Promise<ImageType> {
-        throw new Error("Method not implemented.");
+        const {
+            error: updateImageTypeError,
+            response: updateImageTypeResponse,
+        } = await promisifyGRPCCall(
+            this.imageServiceDM.updateImageType.bind(this.imageServiceDM),
+            { id, displayName, hasPredictiveModel }
+        );
+        if (updateImageTypeError !== null) {
+            this.logger.error(
+                "failed to call image_service.updateImageType()",
+                { error: updateImageTypeError }
+            );
+            throw new ErrorWithHTTPCode(
+                "failed to update image type",
+                getHttpCodeFromGRPCStatus(updateImageTypeError.code)
+            );
+        }
+
+        return ImageType.fromProto(updateImageTypeResponse?.imageType);
     }
 
     public async deleteImageType(id: number): Promise<void> {
-        throw new Error("Method not implemented.");
+        const { error: deleteImageTypeError } = await promisifyGRPCCall(
+            this.imageServiceDM.deleteImageType.bind(this.imageServiceDM),
+            { id }
+        );
+        if (deleteImageTypeError !== null) {
+            this.logger.error(
+                "failed to call image_service.deleteImageType()",
+                { error: deleteImageTypeError }
+            );
+            throw new ErrorWithHTTPCode(
+                "failed to update image type",
+                getHttpCodeFromGRPCStatus(deleteImageTypeError.code)
+            );
+        }
     }
 
     public async addRegionLabelToImageType(
@@ -76,7 +160,25 @@ export class ImageTypeManagementOperatorImpl
         displayName: string,
         color: string
     ): Promise<RegionLabel> {
-        throw new Error("Method not implemented.");
+        const {
+            error: createRegionLabelError,
+            response: createRegionLabelResponse,
+        } = await promisifyGRPCCall(
+            this.imageServiceDM.createRegionLabel.bind(this.imageServiceDM),
+            { ofImageTypeId: imageTypeID, displayName, color }
+        );
+        if (createRegionLabelError !== null) {
+            this.logger.error(
+                "failed to call image_service.createRegionLabel()",
+                { error: createRegionLabelError }
+            );
+            throw new ErrorWithHTTPCode(
+                "failed to create region label",
+                getHttpCodeFromGRPCStatus(createRegionLabelError.code)
+            );
+        }
+
+        return RegionLabel.fromProto(createRegionLabelResponse?.regionLabel);
     }
 
     public async updateRegionLabelOfImageType(
@@ -85,14 +187,53 @@ export class ImageTypeManagementOperatorImpl
         displayName: string | undefined,
         color: string | undefined
     ): Promise<RegionLabel> {
-        throw new Error("Method not implemented.");
+        const {
+            error: updateRegionLabelError,
+            response: updateRegionLabelResponse,
+        } = await promisifyGRPCCall(
+            this.imageServiceDM.updateRegionLabel.bind(this.imageServiceDM),
+            {
+                ofImageTypeId: imageTypeID,
+                id: regionLabelID,
+                displayName,
+                color,
+            }
+        );
+        if (updateRegionLabelError !== null) {
+            this.logger.error(
+                "failed to call image_service.updateRegionLabel()",
+                { error: updateRegionLabelError }
+            );
+            throw new ErrorWithHTTPCode(
+                "failed to update region label",
+                getHttpCodeFromGRPCStatus(updateRegionLabelError.code)
+            );
+        }
+
+        return RegionLabel.fromProto(updateRegionLabelResponse?.regionLabel);
     }
 
     public async removeRegionLabelFromImageType(
         imageTypeID: number,
         regionLabelID: number
-    ): Promise<RegionLabel> {
-        throw new Error("Method not implemented.");
+    ): Promise<void> {
+        const { error: deleteRegionLabel } = await promisifyGRPCCall(
+            this.imageServiceDM.deleteRegionLabel.bind(this.imageServiceDM),
+            {
+                ofImageTypeId: imageTypeID,
+                id: regionLabelID,
+            }
+        );
+        if (deleteRegionLabel !== null) {
+            this.logger.error(
+                "failed to call image_service.updateRegionLabel()",
+                { error: deleteRegionLabel }
+            );
+            throw new ErrorWithHTTPCode(
+                "failed to update region label",
+                getHttpCodeFromGRPCStatus(deleteRegionLabel.code)
+            );
+        }
     }
 }
 
